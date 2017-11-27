24ddf49a1e4b7b2db8a0259c2a25e878cebc0430
Eli Jackson <EliJackson@Elis-MacBook-Pro.local>
2017-11-27 13:54:21 -0600

    added images, made JS changes

---
+++ main.js
@@ -0,0 +1,61 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+for (var i=0; i<products.length; i++) {
+    console.log(products[i]);
+    if (i === 0) {
+        document.getElementById(" ").innerHTML+= '<img src="' + products[i].img + '">' + "<h3>" + products[i].name + "</h3>" + "<br>" + products[i].description + "<br>" + products[i].price + "<br>"
+    } else if (i === 1) {
+        document.getElementById(" ").innerHTML+= '<img src="' + products[i].img + '">' + "<h3>" + products[i].name + "</h3>" + "<br>" + products[i].description + "<br>" + products[i].price + "<br>" 
+    } else if (i === 2) {
+        document.getElementById(" ").innerHTML+= '<img src="' + products[i].img + '">' + "<h3>" + products[i].name + "</h3>" + "<br>" + products[i].description + "<br>" + products[i].price + "<br>" 
+    } else if (i === 3) {
+        document.getElementById(" ").innerHTML+= '<img src="' + products[i].img + '">' + "<h3>" + products[i].name + "</h3>" + "<br>" + products[i].description + "<br>" + products[i].price + "<br>" 
+    } else if (i === 4) {
+        document.getElementById(" ").innerHTML+= '<img src="' + products[i].img + '">' + "<h3>" + products[i].name + "</h3>" + "<br>" + products[i].description + "<br>" + products[i].price + "<br>"
+    } else if (i === 5) {
+        document.getElementById(" ").innerHTML+= '<img src="' + products[i].img + '">' + "<h3>" + products[i].name + "</h3>" + "<br>" + products[i].description + "<br>" + products[i].price + "<br>"  
+    } else if (i === 6) {
+        document.getElementById(" ").innerHTML+= '<img src="' + products[i].img + '">' + "<h3>" + products[i].name + "</h3>" + "<br>" + products[i].description + "<br>" + products[i].price + "<br>"  
+    } else if (i === 7) {
+        document.getElementById(" ").innerHTML+= '<img src="' + products[i].img + '">' + "<h3>" + products[i].name + "</h3>" + "<br>" + products[i].description + "<br>" + products[i].price + "<br>" 
+    }
